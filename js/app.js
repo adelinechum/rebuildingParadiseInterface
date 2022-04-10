49172bc56676ab76159028bfe4b53363413d37be
akv2118 <71468499+akv2118@users.noreply.github.com>
2022-04-10 13:57:45 -0400

    Revert "first person controls"
    This reverts commit 05947009622a57ba48e64583773011f60c165bdd.

--- js/app.js
+++ js/app.js
@@ -4,9 +4,18 @@ import { Color } from '../three.js-master/build/three.module.js';
 import * as THREE from '../three.js-master/examples/build/three.module.js';
 
 import Stats from '../three.js-master/examples/jsm/libs/stats.module.js';
-import { PointerLockControls } from '../three.js-master/examples/jsm/controls/PointerLockControls.js';
+import { OrbitControls } from '../three.js-master/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from '../three.js-master/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from '../three.js-master/examples/jsm/loaders/DRACOLoader.js';
+
+import { Flow } from '../three.js-master/examples/jsm/modifiers/CurveModifier.js';
+import { InstancedFlow } from '../three.js-master/examples/jsm/modifiers/CurveModifier.js';
+
+
+// TO DO 
+//First Person Ref: https://threejs.org/examples/?q=pointer#misc_controls_pointerlock
+//Loading multiple files: https://redstapler.co/load-multiple-model-three-js-promise/
+// for cars https://hofk.de/main/discourse.threejs/2021/MotionAlongCurve/MotionAlongCurve.html
 
 
 var container, camera, controls, scene, renderer;
@@ -15,28 +24,17 @@ const clock = new THREE.Clock();
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 const threshold = 0.1;
-var objects = [];
 
 var liveCameras = []
 var historicalPts = []
 var sinCounter = 0; 
-
-let moveForward = false;
-let moveBackward = false;
-let moveLeft = false;
-let moveRight = false;
-let canJump = false;
-
-let prevTime = performance.now();
-const velocity = new THREE.Vector3();
-const direction = new THREE.Vector3();
-const vertex = new THREE.Vector3();
 
 //center of bounding box
 var center;
 
 //Bounding box to find center
 // const box = new THREE.Box3()
+
 //console.log(window.location.href)
 
 init();
@@ -68,155 +66,44 @@ function init() {
   camera = new THREE.PerspectiveCamera( 30, container.clientWidth / container.clientHeight, 1, 50000 );
 
   // camera.maxDistance= 1000
-  camera.position.set( -1705, 197, 2500); // starting position of the camera
+  camera.position.set( -34178, 6000, 8989); // starting position of the camera
 
   console.log(camera.position)
 
-  controls = new PointerLockControls( camera, document.body );
-
-  const blocker = document.getElementById( 'blocker' );
-  const instructions = document.getElementById( 'instructions' );
-
-  instructions.addEventListener( 'click', function () {
-
-    controls.lock();
-
-  } );
-
-  controls.addEventListener( 'lock', function () {
-
-    instructions.style.display = 'none';
-    blocker.style.display = 'none';
-
-  } );
-
-  controls.addEventListener( 'unlock', function () {
-
-    blocker.style.display = 'block';
-    instructions.style.display = '';
-
-  } );
-
-  scene.add( controls.getObject() );
-
-  const onKeyDown = function ( event ) {
-
-    switch ( event.code ) {
-
-      case 'ArrowUp':
-      case 'KeyW':
-        moveForward = true;
-        break;
-
-      case 'ArrowLeft':
-      case 'KeyA':
-        moveLeft = true;
-        break;
-
-      case 'ArrowDown':
-      case 'KeyS':
-        moveBackward = true;
-        break;
-
-      case 'ArrowRight':
-      case 'KeyD':
-        moveRight = true;
-        break;
-
-      case 'Space':
-        if ( canJump === true ) velocity.y += 350;
-        canJump = false;
-        break;
-
-    }
-
-  };
-
-  const onKeyUp = function ( event ) {
-
-    switch ( event.code ) {
-
-      case 'ArrowUp':
-      case 'KeyW':
-        moveForward = false;
-        break;
-
-      case 'ArrowLeft':
-      case 'KeyA':
-        moveLeft = false;
-        break;
-
-      case 'ArrowDown':
-      case 'KeyS':
-        moveBackward = false;
-        break;
-
-      case 'ArrowRight':
-      case 'KeyD':
-        moveRight = false;
-        break;
-
-    }
-
-  };
-
-  document.addEventListener( 'keydown', onKeyDown );
-  document.addEventListener( 'keyup', onKeyUp );
+  //camera controls to allow for orbiting
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true; // creates a softer orbiting feel
+  controls.dampingFactor = 0.1; // determines how soft
+  controls.enableZoom = true;
+  controls.maxDistance = 33000; // 35847 magnitude of camera position vector
+  //controls.maxZoom = 1;
+  controls.maxPolarAngle = Math.PI / 2;
+  //controls.autoRotate = true;
+  controls.screenSpacePanning = true;
 
 
-  //raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+  // load scene
+  var loader = new THREE.ObjectLoader();
+/*
+  loader.load(
+  	// resource URL
+  	"./assets/groundPoints.json",
+   // "./assets/camerasRenders.json",
 
-  // floor
 
-  let floorGeometry = new THREE.PlaneGeometry( 20000, 20000, 100, 1000 );
-  floorGeometry.rotateX( - Math.PI / 2 );
-
-  // vertex displacement
-
-  let position = floorGeometry.attributes.position;
-
-  for ( let i = 0, l = position.count; i < l; i ++ ) {
-
-    vertex.fromBufferAttribute( position, i );
-
-    vertex.x += Math.random() * 20 - 10;
-    vertex.y += Math.random() * 2;
-    vertex.z += Math.random() * 20 - 10;
-
-    position.setXYZ( i, vertex.x, vertex.y, vertex.z );
-
-  }
-
-  const floorMaterial = new THREE.MeshBasicMaterial({'color':'grey'})
-
-  const floor = new THREE.Mesh( floorGeometry, floorMaterial );
-  scene.add( floor );
-
-// load scene
-const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath( '../three.js-master/examples/js/libs/draco/gltf/' );
-
-    const gltfloader = new GLTFLoader();
-    gltfloader.setDRACOLoader( dracoLoader );
-    gltfloader.load( './assets/TriceratopsStyleExport5.glb', function ( gltf ) {
-
-      const model = gltf.scene;
-      model.position.set( 1, 1, 0 );
-      model.scale.set( 0.05, 0.05, 0.05 );
-      scene.add( model );
-      scene.fog = new THREE.Fog( 'black', 20, 3000 );
-
-      mixer = new THREE.AnimationMixer( model );
-      mixer.clipAction( gltf.animations[ 0 ] ).play();
-
+  	// onLoad callback
+  	function ( obj ) {
+      // remove the loading text
+      document.getElementById('progress').remove();
+      
+  		// assign the loaded object to the scene variable
+  		scene = obj;
       console.log(scene.children);
 
-      // adding all the points in the "Scene" mesh
-      // TODO: this is breakable!!!
-      objects = scene.children[2].children[0].children;
+      console.log(scene);
+      scene.fog = new THREE.Fog( 'black', 20, 50000 );
 
-
-/*       //bounding box to get center of objects
+      //bounding box to get center of objects
       var bbox = new THREE.Box3().setFromObject(obj);
       //console.log(bbox);
       center = new THREE.Vector3();
@@ -238,7 +125,37 @@ const dracoLoader = new DRACOLoader();
         if (child.name.match('^historicalPt')) {
           historicalPts.push(child);
         }
-      }); */
+      });
+
+  	},
+
+  	// onProgress callback
+  	 function ( xhr ) {
+      progressText( xhr ) // delete this if you don't want the progress text
+  	 },
+
+  	// onError callback
+  	function ( err ) {
+  		console.error( 'An error happened' );
+      console.log('error found');
+      console.log('ERROR FOUND: ' + err);
+  	}
+  );
+*/
+const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath( '../three.js-master/examples/js/libs/draco/gltf/' );
+
+    const gltfloader = new GLTFLoader();
+    gltfloader.setDRACOLoader( dracoLoader );
+    gltfloader.load( './assets/TriceratopsStyleExport5.glb', function ( gltf ) {
+
+      const model = gltf.scene;
+      model.position.set( 1, 1, 0 );
+      model.scale.set( 0.01, 0.01, 0.01 );
+      scene.add( model );
+
+      mixer = new THREE.AnimationMixer( model );
+      mixer.clipAction( gltf.animations[ 0 ] ).play();
 
       animate();
 
@@ -260,7 +177,7 @@ const dracoLoader = new DRACOLoader();
   
     mixer.update( delta );
   
-    //controls.update();
+    controls.update();
   
     stats.update();
   
@@ -269,6 +186,8 @@ const dracoLoader = new DRACOLoader();
   }
 
 }
+
+
 
 // adds progress text while the model is loading
 function progressText( xhr ) {
@@ -313,70 +232,19 @@ function animate() {
   sinCounter = sinCounter + (Math.PI / 32);
 
   requestAnimationFrame( animate );
-
-  const time = performance.now();
-
-  if ( controls.isLocked === true ) {
-
-    raycaster.ray.origin.copy( controls.getObject().position );
-    raycaster.ray.origin.y -= 10;
-
-    const intersections = raycaster.intersectObjects( objects, false );
-
-    const onObject = intersections.length > 0;
-
-    const delta = ( time - prevTime ) / 1000;
-
-    velocity.x -= velocity.x * 1.0 * delta;
-    velocity.z -= velocity.z * 1.0 * delta;
-
-    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-
-    direction.z = Number( moveForward ) - Number( moveBackward );
-    direction.x = Number( moveRight ) - Number( moveLeft );
-    direction.normalize(); // this ensures consistent movements in all directions
-
-    if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
-    if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
-
-    if ( onObject === true ) {
-      console.log("intesected with object!");
-
-      velocity.y = Math.max( 0, velocity.y );
-      canJump = true;
-
-    }
-
-    controls.moveRight( - velocity.x * delta );
-    controls.moveForward( - velocity.z * delta );
-
-    controls.getObject().position.y += ( velocity.y * delta ); // new behavior
-
-    if ( controls.getObject().position.y < 10 ) {
-
-      velocity.y = 0;
-      controls.getObject().position.y = 10;
-
-      canJump = true;
-
-    }
-
-  }
   
-  //controls.update();
+  controls.update();
 
   // live cam & historical points animate
   liveCameras.forEach(element => {
     element.rotation.y -= 0.05;
   });  
-
+//TO DO change to move up and down
   historicalPts.forEach(element => {
     //element.rotation.y -= 0.05;
     element.translateY((Math.sin(sinCounter) * 25));
   });  
-  
-  prevTime = time;
-  //console.log(camera.position)
+ 
   render();
 
 }
@@ -390,11 +258,11 @@ function render() {
   raycaster.params.Points.threshold = 10; // don't know why threshold this high
 
   for ( let i = 0; i < intersects.length; i ++ ) {
-    // console.log(intersects [i] ); // this is not printing
-    // intersects[i].object.material.color.set ("red");
+    console.log(intersects [i] ); // this is not printing
+    //intersects[i].object.material.color.set ("red");
 
   }
 
-  //controls.update( clock.getDelta() );
+  controls.update( clock.getDelta() );
   renderer.render( scene, camera ); 
 }
